4ba5c8a06e8538b18683bb5093c39fc82fec2937
Ayush <ayus202007@gmail.com>
2026-09-07 00:48:17 +0530

    Update 0268-missing-number.cpp

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -3,9 +3,9 @@ public:
     int missingNumber(vector<int>& nums) {
         
         int n = nums.size();
-        int expected = n *(n+1)/2 ;
+        int expected = n *(n+1)/2 ;//expectedsum
         //n*(n+1)/2 is the sum of 1 to n numbers
-        int actual=0;
+        int actual=0;//actualsum
         for(int i = 0 ; i<n ; i++){
 
             actual = actual + nums[i];
