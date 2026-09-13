94b6f5c01ce3808e1426419edbdde34b2e93b442
Ayush <ayus202007@gmail.com>
2026-09-14 00:33:27 +0530

    Time: 0 ms (100%), Space: 15.2 MB (34.07%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -12,7 +12,14 @@ public:
                 brr[i][j] = matrix[j][i];
             }
         }
-
+        
         return brr;
+        // int n = matrix.size();
+
+//         for(int i = 0; i < n; i++) {
+//             for(int j = i + 1; j < n; j++) {
+//                 swap(matrix[i][j], matrix[j][i]);
+//             }
+
     }
 };
